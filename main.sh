98470500b8a063965e756799a4c48ae0c3f07f80
Jack Yu <jackchungchiehyu@gmail.com>
2020-08-07 13:58:43 +0000

    main.sh to train 5 epochs, then 5 epochs

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-python src/train.py -n 2 -g 1 -nr 0 --input_dir=./datasets --checkpoint_dir=./results/checkpoints_test --sample_dir=./results/samples_test --is_perceptual_oriented=True --scale_factor=4 --batch_size=16 --epoch=0 --num_epoch=1
+python src/train.py -n 2 -g 1 -nr 0 --input_dir=./datasets --checkpoint_dir=./results/checkpoints_test --sample_dir=./results/samples_test --is_perceptual_oriented=True --scale_factor=4 --batch_size=16 --epoch=0 --num_epoch=5
 
-python src/train.py -n 2 -g 1 -nr 0 --input_dir=./datasets --checkpoint_dir=./results/checkpoints_test --sample_dir=./results/samples_test --is_perceptual_oriented=False --scale_factor=4 --batch_size=16 --epoch=1 --num_epoch=2
+python src/train.py -n 2 -g 1 -nr 0 --input_dir=./datasets --checkpoint_dir=./results/checkpoints_test --sample_dir=./results/samples_test --is_perceptual_oriented=False --scale_factor=4 --batch_size=16 --epoch=5 --num_epoch=10
