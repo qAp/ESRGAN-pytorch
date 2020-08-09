c76a4551616f6909b7128a86f1420dbed10545ad
Jack Yu <jackchungchiehyu@gmail.com>
2020-08-09 03:18:22 +0000

    Fix bug

--- src/train.py
+++ src/train.py
@@ -50,14 +50,10 @@ class Trainer:
         self.generator.train()
         self.discriminator.train()
 
-        print(f"{'epoch':>7s}"
-              f"{'batch':>7s}"
-              f"{'D loss':>15s}"
-              f"{'G loss':>15s}"
-              f"{'adver. loss':>15s}"
-              f"{'percp. loss':>15s}"
-              f"{'contn. loss':>15s}"
-              f"")
+        print(f"{'epoch':>7s}"  f"{'batch':>7s}" f"{'discr.':>10s}"
+              f"{'gener.':>10s}" f"{'adver.':>10s}" f"{'percp.':>10s}"
+              f"{'contn.':>10s}" f"")
+        
         for epoch in range(args.epoch, args.num_epoch):
             sample_dir_epoch = Path(args.checkpoint_dir)/'sample_dir'/str(epoch)
             sample_dir_epoch.mkdir(exist_ok=True, parents=True)
@@ -126,8 +122,8 @@ class Trainer:
                 self.lr_scheduler_discriminator.step()
                 
                 if step % 1000 == 0:
-                    print(f"{epoch:>3d}/{args.num_epoch:<3d}"
-                          f"{step:>3d}/{total_step:<3d}"
+                    print(f"{epoch:>3d}:{args.num_epoch:<3d}"
+                          f"{step:>3d}:{total_step:<3d}"
                           f"{discriminator_loss.item():>10.4f}"
                           f"{generator_loss.item():>10.4f}"
                           f"{adversarial_loss.item()*self.adversarial_loss_factor:>10.4f}"
@@ -135,14 +131,10 @@ class Trainer:
                           f"{content_loss.item()*self.content_loss_factor:>10.4f}"
                           f"")
                     if step % 5000 == 0:
-                        print(f"{'epoch':>7s}"
-                              f"{'batch':>7s}"
-                              f"{'D loss':>15s}"
-                              f"{'G loss':>15s}"
-                              f"{'adver. loss':>15s}"
-                              f"{'percp. loss':>15s}"
-                              f"{'contn. loss':>15s}"
-                              f"")
+                        print(f"{'epoch':>7s}"  f"{'batch':>7s}" f"{'discr.':>10s}"
+                              f"{'gener.':>10s}" f"{'adver.':>10s}" f"{'percp.':>10s}"
+                              f"{'contn.':>10s}" f"")
+                        
                         result = torch.cat((high_resolution, fake_high_resolution), 2)
                         save_image(result, sample_dir_epoch/f"SR_{step}.png")
 
