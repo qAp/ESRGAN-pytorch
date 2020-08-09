5c25f2c23923d79444f8f8d7ecc4e78492d45522
Jack Yu <jackchungchiehyu@gmail.com>
2020-08-09 10:19:03 +0000

    Revert "Make distributed training optional"
    This reverts commit 30c3c1db1427e7743d611c163994c8bc62f8a7a3.

--- src/dataloader/dataloader.py
+++ src/dataloader/dataloader.py
@@ -5,13 +5,9 @@ import torch
 
 def get_loader(args):
     train_dataset = Datasets(args.image_size, args.scale_factor, args.input_dir)
-    if args.distributed:
-        train_sampler = torch.utils.data.distributed.DistributedSampler(
-            train_dataset, num_replicas=args.world_size, rank=args.rank)
-        train_loader = torch.utils.data.DataLoader(
-            dataset=train_dataset,
-            batch_size=args.batch_size, shuffle=False, sampler=train_sampler)
-    else:
-        train_loader = torch.utils.data.DataLoader(
-            dataset=train_dataset, batch_size=args.batch_size, shuffle=True)
+    train_sampler = torch.utils.data.distributed.DistributedSampler(
+        train_dataset, num_replicas=args.world_size, rank=args.rank)
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset,
+        batch_size=args.batch_size, shuffle=False, sampler=train_sampler)
     return train_loader
