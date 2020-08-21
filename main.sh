8ff0e60c1603fe77c24b38b425aa180aaaea07a2
Jack Yu <jackchungchiehyu@gmail.com>
2020-08-21 05:38:25 +0000

    Set up exp15

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-python src/train.py -n 2 -g 1 -nr 0 --input_dir=./datasets --load=./parameters/psnr_based.pth --checkpoint_dir=./results/exp14  --batch_size=16 --epoch=0 --num_epoch=500 --is_perceptual_oriented=True --scale_factor=4 --fp16 --distributed
+python src/train.py -n 1 -g 1 -nr 0 --input_dir=./datasets --resume=./results/exp14/best.pth --checkpoint_dir=./results/exp15  --batch_size=16 --epoch=0 --num_epoch=700 --is_perceptual_oriented=False --scale_factor=4 --fp16
 
 #python src/train.py -n 1 -g 1 -nr 0 --input_dir=./datasets --resume=./results/exp11/last.pth --checkpoint_dir=./results/exp12  --batch_size=16 --epoch=0 --num_epoch=800 --fp16 --is_perceptual_oriented=False --scale_factor=4 
 
