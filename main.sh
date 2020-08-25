61d6ded3aac1e314b136c83e726dea2a2eee6248
Jack Yu <jackchungchiehyu@gmail.com>
2020-08-25 06:58:53 +0000

    Set up exp16

--- main.sh
+++ main.sh
@@ -3,16 +3,16 @@
 
 
 
-# Example: Start PSNR-based training from library's PSNR-based model
-python src/train.py -n 2 -g 1 -nr 0 --input_dir=./datasets --load=./parameters/psnr_based.pth --checkpoint_dir=./results/exp14  --batch_size=16 --epoch=0 --num_epoch=350 --is_perceptual_oriented=True --scale_factor=4 --fp16 --distributed
+# Example: Start PSNR-based training.
+python src/train.py -n 2 -g 1 -nr 0 --input_dir=./datasets --load=./parameters/gan_based.pth --checkpoint_dir=./results/exp16  --batch_size=16 --epoch=0 --num_epoch=350 --is_perceptual_oriented=True --scale_factor=4 --fp16 --distributed
 
-# Example: Resume a PSNR-based training.
+# Example: Resume  PSNR-based training.
 #python src/train.py -n 2 -g 1 -nr 0 --input_dir=./datasets --resume=./results/exp14/last.pth --checkpoint_dir=./results/exp14  --batch_size=16 --epoch=0 --num_epoch=350 --is_perceptual_oriented=True --scale_factor=4 --fp16 --distributed
 
-# Example: Start GAN-based from PSNR-based
+# Example: Start GAN-based training.
 # python src/train.py -n 2 -g 1 -nr 0 --input_dir=./datasets --load=./results/exp14/best.pth --checkpoint_dir=./results/exp15  --batch_size=16 --epoch=0 --num_epoch=200 --is_perceptual_oriented=False --scale_factor=4 --fp16 --distributed
 
-# Resume example: GAN-based
+# Example: Resume GAN-based training.
 # python src/train.py -n 2 -g 1 -nr 0 --input_dir=./datasets --resume=./results/exp15/last.pth --checkpoint_dir=./results/exp15  --batch_size=16 --epoch=0 --num_epoch=200 --is_perceptual_oriented=False --scale_factor=4 --fp16 --distributed
 
 #python src/train.py -n 1 -g 1 -nr 0 --input_dir=./datasets --resume=./results/exp11/last.pth --checkpoint_dir=./results/exp12  --batch_size=16 --epoch=0 --num_epoch=800 --fp16 --is_perceptual_oriented=False --scale_factor=4 
